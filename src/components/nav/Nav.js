96a6b6742e1ad1c47b3f8dc3c71e003334869f72
lliolla <lliolla@hotmail.com>
2023-03-06 14:18:54 +0100

    creation du portofolio et fin du projet

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BsFiles} from 'react-icons/bs'
 
-export const Nav = () => {
+import {useState} from 'react'
+
+const Navigation = () => {
+
+const [activeNav, setActiveNav] = useState('#home')
+
   return (
-    <div>nav</div>
+    <nav>
+       <a href="#home" onClick={() => setActiveNav('#home')}className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome/></a>
+       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsFiles/></a>
+       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+
+    </nav>
   )
 }
-export default Nav
+
+export default Navigation
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,9 +1,40 @@
 import React from 'react'
 import './portfolio.css'
+import {PortfolioData} from './PortfolioData'
 
-export const Portfolio = () => {
+
+
+const Portfolio = () => {
   return (
-    <div>portfolio</div>
+    <section id="portfolio">
+      <h5>
+        My Recent Work
+      </h5>
+      <h2>
+        Portfolio
+      </h2>
+      <div className="container portfolio__container">
+        {
+          PortfolioData.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                   <img src={image} alt={title} />
+                </div>
+                <h3>
+                  {title}
+                </h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn">Github</a>
+                  <a href={demo} target="_blank" rel="noreferrer" className="btn btn-primary">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
+
 export default Portfolio
